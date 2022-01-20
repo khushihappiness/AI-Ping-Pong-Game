ef85f1243b5225745fd7e597129ff5f26e5622fc
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2022-01-20 15:42:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,7 +23,21 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+
+  video=createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  posenet=ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+  console.log("Model is loaded");
+}
+
 
 
 function draw(){
